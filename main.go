39efa0b7d75776c0e05bf50beb9185096011df72
HeartCoder <34908308+surajNirala@users.noreply.github.com>
2024-05-01 18:25:42 +0530

    Update email notification when fail build main.go

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main(){
   fmt.Println("SCM Jenkins")
+  fmt.Println("Error found")
   for i:=1;i<=10;i++{
-    fmt.Println("number :===== ", i)
+    fmt.Println"number :===== ", i)
   }
 }
