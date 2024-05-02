d0bedf8dd587ea4a5283486263a070a9a8125ff2
HeartCoder <34908308+surajNirala@users.noreply.github.com>
2024-05-02 14:51:11 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main(){
-  fmt.Println("SCM Jenkins")
+  fmt.Println("SCM Jenkinsjjj")
   fmt.Println("Error found")
   for i:=1;i<=10;i++{
     fmt.Println ("number Error :===== ", i
