5a8c33b42fee857448b8291f3a3e4b2e834e0250
HeartCoder <34908308+surajNirala@users.noreply.github.com>
2024-05-06 18:59:52 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main(){
-  fmt.Println("SCM Jenkinsjjj")
-  fmt.Println("Error found")
+  fmt.Println("SCM Working")
+  fmt.Println("Auto  found")
   for i:=1;i<=10;i++{
-    fmt.Println ("Number :== ", i)
+    fmt.Println ("Number := ", i)
   }
 }
