48a1e0b203d54139ce9b98d115598a8519e59cc2
HeartCoder <34908308+surajNirala@users.noreply.github.com>
2024-05-06 19:02:57 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main(){
-  fmt.Println("SCM Working")
+  fmt.Println("SCM Working Update Correct Change")
   fmt.Println("Auto  found")
   for i:=1;i<=10;i++{
-    fmt.Println ("Number := ", i
+    fmt.Println ("Number := ", i)
   }
 }
